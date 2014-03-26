fbf3ac77ed6b5a3a49b923cae2da54ec400f3b44
jeff66ruan <jeff66ruan@gmail.com>
2014-03-26 08:46:53 +0800

    first commit

---
+++ acs.el
@@ -0,0 +1,76 @@
+;;; acs.el --- A front-end for codesearch
+
+;; Copyright (C) 2014 Jeff Ruan <jeff66ruan@gmail.com>
+;;
+;; Author: Jeff Ruan
+
+
+;;; Commentary:
+
+;; 
+;; 
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+(require 'cl) 
+
+(defcustom acs-cindex
+  "cindex"
+  "Name of the codesearch cindex executable to use."
+  :type 'string
+  :group 'acs)
+
+(defcustom acs-csearch
+  "csearch"
+  "Name of the codesearch csearch executable to use."
+  :type 'string
+  :group 'acs)
+
+(require 'thingatpt) ;; thing-at-point
+
+(defun acs-region-or-symbol-at-point ()
+  "Return an active selection or the symbol at point as a
+   string. "
+  (cond ((use-region-p)
+         (buffer-substring-no-properties (region-beginning) (region-end)))
+        ((symbol-at-point)
+         (substring-no-properties
+          (symbol-name (symbol-at-point))))))
+
+(defun acs (string directory)
+  "Search in a given DIRECTORY or files for a given search STRING,
+with STRING defaulting to the symbol under point. Both DIRECTORY
+and string is PATTERN"
+  (interactive (list (read-from-minibuffer "Search string: " (acs-region-or-symbol-at-point))
+                      (read-directory-name "Directory: ")))
+  (let ((commands nil)
+	(full-directory (expand-file-name directory))
+	(switch-to-visible-buffer t))
+    (setq commands (list acs-csearch "-f" full-directory "-n" string))
+    (setq commands (mapconcat 'shell-quote-argument commands " "))
+    (print commands)
+    (shell-command commands "*codesearch*")
+    (pop-to-buffer "*codesearch*")
+    (compilation-mode)))
+
+
+(provide 'acs)
